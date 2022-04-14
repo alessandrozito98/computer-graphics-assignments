9ff0900a1babd62ab16d7c514b9d3ae7c507cf03
Alessandro Zito <alessandro4.zito@mail.polimi.it>
2022-04-14 22:20:54 +0200

    feat: A06

---
+++ A06/shaders/TextShader.frag
@@ -0,0 +1,16 @@
+#version 450#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) in vec2 fragTexCoord;
+
+layout(location = 0) out vec4 outColor;
+
+layout(binding = 1) uniform sampler2D texSampler;
+
+const vec4 FGcolor = vec4(1.0f);
+const vec4 BGcolor = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+const vec4 SHcolor = vec4(0.0f, 0.0f, 0.0f, 0.0f);
+
+void main() {
+	vec4 Tx = texture(texSampler, fragTexCoord);
+	outColor = Tx.r * FGcolor + Tx.g * BGcolor + Tx.b * SHcolor;
+}
---
+++ A06/view.cpp
@@ -0,0 +1,29 @@
+using namespace glm;
+
+// Create a look in direction matrix
+ // Pos    -> Position of the camera
+ // Angs.x -> direction (alpha)
+ // Angs.y -> elevation (beta)
+ // Angs.z -> roll (rho)
+glm::mat4 LookInDirMat(glm::vec3 Pos, glm::vec3 Angs) {
+
+	mat4 out = glm::mat4(1.0);
+
+	out = glm::rotate(glm::mat4(1.0), -Angs.z, glm::vec3(0, 0, 1)) * glm::rotate(out, -Angs.y, glm::vec3(1, 0, 0)) * 
+		glm::rotate(glm::mat4(1.0), -Angs.x, glm::vec3(0, 1, 0)) * translate(glm::mat4(1.0), -Pos);
+	return out;
+}
+
+ // Create a look at matrix
+ // Pos    -> Position of the camera (c)
+ // aim    -> Posizion of the target (a)
+ // Roll   -> roll (rho)
+glm::mat4 LookAtMat(glm::vec3 Pos, glm::vec3 aim, float Roll) {
+	glm::mat4 out = glm::mat4(1.0);
+
+	out = lookAt(Pos,aim, glm::vec3(0, 1, 0)) * rotate(out, radians(-Roll), vec3(0, 1, 0));
+	return out;
+}
+
+
+
